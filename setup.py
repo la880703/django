194f15d6448d3df4165350e4ab3ece2c04b370e5
hellohuizhang <hellohuizhang@126.com>
2012-08-31 17:23:49 +0800

    first check in

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!python
+
+from distutils.core import setup
+
+setup(
+    name = 'cubrid_django',
+    version = '8.4.1',
+    description = 'CUBRID database backend for Django',
+    author = 'zhanghui',
+    author_email = 'zhanghui@nhn.com',
+    url = 'http://www.cubrid.org',
+    packages = ['cubrid_django'],
+)
